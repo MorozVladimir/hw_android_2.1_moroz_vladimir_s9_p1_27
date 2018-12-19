86edb0c9f7f0cbd4da2e1ee50d7986059d1f38fd
Vladimir Moroz <morozvvlad@gmail.com>
2018-12-19 20:27:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/study_book/hw_android_21_moroz_vladimir_s9_p1_27/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.study_book.hw_android_21_moroz_vladimir_s9_p1_27;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
